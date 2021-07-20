9b1ea98177d2d840ff4beb5d117f66ec264b05da
Ransy-984 <ransyeliasv@gmail.com>
2021-07-20 16:26:56 -0400

    subiendo trabajo

---
+++ Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿
+namespace Proyecto_Final_2
+{
+    partial class ArticleList
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtProductName = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtStock = new System.Windows.Forms.TextBox();
+            this.txtHall = new System.Windows.Forms.TextBox();
+            this.txtExpirationDate = new System.Windows.Forms.TextBox();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.dtgArticle = new System.Windows.Forms.DataGridView();
+            this.btnAll = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgArticle)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(349, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Article List CRUD";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(59, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Product Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(59, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 28);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Price";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(59, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 28);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Stock";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(59, 239);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 28);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Hall";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(59, 285);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(146, 28);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Expiration Date";
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.Location = new System.Drawing.Point(259, 83);
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.Size = new System.Drawing.Size(200, 27);
+            this.txtProductName.TabIndex = 6;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(259, 137);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(200, 27);
+            this.txtPrice.TabIndex = 7;
+            // 
+            // txtStock
+            // 
+            this.txtStock.Location = new System.Drawing.Point(259, 190);
+            this.txtStock.Name = "txtStock";
+            this.txtStock.Size = new System.Drawing.Size(200, 27);
+            this.txtStock.TabIndex = 8;
+            // 
+            // txtHall
+            // 
+            this.txtHall.Location = new System.Drawing.Point(259, 243);
+            this.txtHall.Name = "txtHall";
+            this.txtHall.Size = new System.Drawing.Size(200, 27);
+            this.txtHall.TabIndex = 9;
+            // 
+            // txtExpirationDate
+            // 
+            this.txtExpirationDate.Location = new System.Drawing.Point(259, 286);
+            this.txtExpirationDate.Name = "txtExpirationDate";
+            this.txtExpirationDate.Size = new System.Drawing.Size(200, 27);
+            this.txtExpirationDate.TabIndex = 10;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnInsert.Location = new System.Drawing.Point(587, 96);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(104, 47);
+            this.btnInsert.TabIndex = 11;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnClear.Location = new System.Drawing.Point(760, 95);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(104, 47);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnUpdate.Location = new System.Drawing.Point(587, 191);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(104, 47);
+            this.btnUpdate.TabIndex = 13;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnDelete.Location = new System.Drawing.Point(760, 190);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(104, 47);
+            this.btnDelete.TabIndex = 14;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnSearch.Location = new System.Drawing.Point(587, 297);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(105, 45);
+            this.btnSearch.TabIndex = 15;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(59, 349);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 28);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Id";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(259, 353);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(200, 27);
+            this.txtId.TabIndex = 17;
+            // 
+            // dtgArticle
+            // 
+            this.dtgArticle.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgArticle.Location = new System.Drawing.Point(59, 408);
+            this.dtgArticle.Name = "dtgArticle";
+            this.dtgArticle.RowHeadersWidth = 51;
+            this.dtgArticle.RowTemplate.Height = 29;
+            this.dtgArticle.Size = new System.Drawing.Size(805, 170);
+            this.dtgArticle.TabIndex = 18;
+            // 
+            // btnAll
+            // 
+            this.btnAll.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnAll.Location = new System.Drawing.Point(759, 294);
+            this.btnAll.Name = "btnAll";
+            this.btnAll.Size = new System.Drawing.Size(105, 45);
+            this.btnAll.TabIndex = 19;
+            this.btnAll.Text = "All";
+            this.btnAll.UseVisualStyleBackColor = true;
+            this.btnAll.Click += new System.EventHandler(this.btnAll_Click);
+            // 
+            // ArticleList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(930, 603);
+            this.Controls.Add(this.btnAll);
+            this.Controls.Add(this.dtgArticle);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.txtExpirationDate);
+            this.Controls.Add(this.txtHall);
+            this.Controls.Add(this.txtStock);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtProductName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ArticleList";
+            this.Text = "Article List";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgArticle)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtProductName;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtStock;
+        private System.Windows.Forms.TextBox txtHall;
+        private System.Windows.Forms.TextBox txtExpirationDate;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.DataGridView dtgArticle;
+        private System.Windows.Forms.Button btnAll;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Final_2
+{
+    public partial class ArticleList : Form
+    {
+        public ArticleList()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtProductName.Text) || string.IsNullOrEmpty(txtPrice.Text) || string.IsNullOrEmpty(txtStock.Text) || string.IsNullOrEmpty(txtExpirationDate.Text) || string.IsNullOrEmpty(txtHall.Text))
+            {
+                MessageBox.Show("One or more parameters are missing");
+            }
+            else
+            {
+                SqlConnection con = new SqlConnection("Data Source=LAPTOP-N3ABNDAC;Initial Catalog=Bravo;User ID=bravo;Password=bravo");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("INSERT INTO ListArticle VALUES(@ProductName, @Price, @Stock, @Hall, @ExpirationDate) ", con);
+                cmd.Parameters.AddWithValue("@ProductName", txtProductName.Text);
+                cmd.Parameters.AddWithValue("@Price", Convert.ToDouble(txtPrice.Text));
+                cmd.Parameters.AddWithValue("@Stock", int.Parse(txtStock.Text));
+                cmd.Parameters.AddWithValue("@Hall", int.Parse(txtHall.Text));
+                cmd.Parameters.AddWithValue("@ExpirationDate", Convert.ToDateTime(txtExpirationDate.Text));
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+                MessageBox.Show("Article succsesfully Inserted");
+            }
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtProductName.Clear();
+            txtPrice.Clear();
+            txtStock.Clear();
+            txtHall.Clear();
+            txtExpirationDate.Clear();
+            txtId.Clear();
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtId.Text) || !typeCheckInt(txtId.Text))
+            {
+                MessageBox.Show("unable to do operation due to lack of data or incorrect parameters ");
+            }
+            else
+            {
+                SqlConnection con = new SqlConnection("Data Source=LAPTOP-N3ABNDAC;Initial Catalog=Bravo;User ID=bravo;Password=bravo");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("UPDATE ListArticle SET ProductName = @ProductName, Price = @Price, Stock = @Stock, Hall = @Hall, ExpirationDate = @ExpirationDate WHERE Id = @Id ", con);
+                cmd.Parameters.AddWithValue("Id", int.Parse(txtId.Text));
+                cmd.Parameters.AddWithValue("@ProductName", txtProductName.Text);
+                cmd.Parameters.AddWithValue("@Price", Convert.ToDouble(txtPrice.Text));
+                cmd.Parameters.AddWithValue("@Stock", int.Parse(txtStock.Text));
+                cmd.Parameters.AddWithValue("@Hall", int.Parse(txtHall.Text));
+                cmd.Parameters.AddWithValue("@ExpirationDate", Convert.ToDateTime(txtExpirationDate.Text));
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+                MessageBox.Show("Article succsesfully Updated");
+            }
+
+
+        }
+
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtId.Text) || !typeCheckInt(txtId.Text))
+            {
+                MessageBox.Show("unable to do operation due to lack of data or incorrect parameters ");
+            }
+            else
+            {
+                SqlConnection con = new SqlConnection("Data Source=LAPTOP-N3ABNDAC;Initial Catalog=Bravo;User ID=bravo;Password=bravo");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("DELETE ListArticle WHERE Id = @Id ", con);
+                cmd.Parameters.AddWithValue("@Id", int.Parse(txtId.Text));
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Article succsesfully Deleted");
+            }
+
+
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtId.Text) || !typeCheckInt(txtId.Text))
+            {
+                MessageBox.Show("unable to do operation due to lack of data or incorrect parameters ");
+            }
+            else
+            {
+                SqlConnection con = new SqlConnection("Data Source=LAPTOP-N3ABNDAC;Initial Catalog=Bravo;User ID=bravo;Password=bravo");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM ListArticle WHERE Id = @Id", con);
+                cmd.Parameters.AddWithValue("Id", int.Parse(txtId.Text));
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dtgArticle.DataSource = dt;
+            }
+
+        }
+
+        private void btnAll_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=LAPTOP-N3ABNDAC;Initial Catalog=Bravo;User ID=bravo;Password=bravo");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM ListArticle" , con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dtgArticle.DataSource = dt;
+        }
+
+
+        private bool typeCheckInt(string text)
+        {
+            int numero = 0;
+            return int.TryParse(text, out numero);
+        }
+        private bool IsNullOrEmpty(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
